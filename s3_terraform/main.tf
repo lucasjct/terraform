ce9c7d97aed399f8b5ab5fd46cca63192e262f0d
Lucas Teixeira <38733166+lucasjct@users.noreply.github.com>
2022-10-24 01:23:06 -0300

    main.tf

---
+++ s3_terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = "v1.3.3"
+
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.36.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.s3_region
+}
+
+resource "aws_s3_bucket" "test-bucket-flp" {
+  bucket = var.s3_bucket
+  acl    = var.acl_s3
+  tags = var.tags_s3
+
+}
