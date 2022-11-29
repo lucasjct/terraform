96e7d2504f4327fd1b3e62599440583c2708819f
Projeto 22 <qa.projeto22@gmail.com>
2022-11-29 01:20:52 -0300

    aws-route-table

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -39,3 +39,20 @@ resource "aws_subnet" "public-subnet-2" {
   }
 }
 
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "Public route table"
+  }
+}
+
+resource "aws_route_table_association" "public-subnet-1-route-table-association-1" {
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-route-table.id
+}
